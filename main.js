ad1b95a0a925b876f99df752547796a2e476ab43
capecod40 <tonyzh1120@gmail.com>
2023-06-06 16:20:38 -0700

    Changed render size

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ scene.background = new THREE.Color(0xc7dcff);
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer({ canvas: document.querySelector(".room-canvas"), antialias: true });
-renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setSize( window.innerWidth * 0.9, window.innerHeight * 0.9 );
 
 // document.body.appendChild( renderer.domElement );
 
@@ -75,7 +75,7 @@ function animate() {
 	let aspect = window.innerWidth / window.innerHeight;
 	camera.aspect = aspect;
 	camera.updateProjectionMatrix();
-	renderer.setSize(window.innerWidth, window.innerHeight);
+	renderer.setSize(window.innerWidth * 0.9, window.innerHeight * 0.9);
 	renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 	renderer.render( scene, camera );
